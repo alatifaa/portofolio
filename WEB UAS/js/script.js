029134f8b865e606cb3f3e79e46e37cbaca40c16
alatifaa <alatifanissa@gmail.com>
2025-12-26 00:52:21 +0700

    first commit

---
+++ WEB UAS/js/script.js
@@ -0,0 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+
+    if (mobileMenu.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
+});
+
+const mobileLinks = document.querySelectorAll('.mobile-nav-links a');
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        mobileMenu.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    });
+});
